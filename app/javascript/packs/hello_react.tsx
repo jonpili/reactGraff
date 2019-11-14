e3a3b0d08ac75310fe30329b98f7dadd7d2361b1
JoeTakasuna <jonpili99@gmail.com>
2019-11-14 15:42:27 +0900

    ボタンでリフトアップできるように

--- app/javascript/packs/hello_react.tsx
+++ app/javascript/packs/hello_react.tsx
@@ -4,28 +4,35 @@ import ReactDOM from "react-dom";
 interface RegisterProps {
   name: string;
   description: string;
+  onClick: (name: string, description: string) => void;
 }
 
-class Register extends React.Component<{}, RegisterProps> {  
+class Register extends React.Component<RegisterProps, {}> {
   render() {
     return (
-      <form>
-        <div>
-          <label>
-            Name:
-            <input type="text" name="name" />
-          </label>
-        </div>
-        <div>
-          <label>
-            Description:
-            <textarea name="description" />
-          </label>
-        </div>
-        <input type="submit" value="Submit" />
+      // <form>
+      //   <div>
+      //     <label>
+      //       Name:
+      //       <input type="text" name="name" />
+      //     </label>
+      //   </div>
+      //   <div>
+      //     <label>
+      //       Description:
+      //       <textarea name="description" />
+      //     </label>
+      //   </div>
+      //   <input type="submit" value="Submit" />
+      //   <button value="confirm" onClick={() => this.props.onClick('おにぎり', '愛でにぎる')}>レシピの確認</button>
+      //   <div>name: {this.props.name}</div>
+      //   <div>description: {this.props.description}</div>
+      // </form>
+      <div>
+        <button onClick={() => this.props.onClick('おにぎり', '愛でにぎる')}>レシピの確認</button>
         <div>name: {this.props.name}</div>
         <div>description: {this.props.description}</div>
-      </form>
+      </div>
     )
   }
 }
@@ -34,27 +41,40 @@ interface MainProps {
   chef: string;
 }
 
-// const Main: React.SFC<MainProps> = props => (
-//   <div>Main {props.name}!</div>
-// )
+interface MainState {
+  name: string;
+  description: string;
+}
 
-class Main extends React.Component<{}, MainProps> {
+class Main extends React.Component<MainProps, MainState> {
+  constructor(props: Readonly<MainProps>) {
+    super(props);
+    this.state = {
+      name: 'オムライス',
+      description: '卵でつつむ'
+    };
+  }
+
+  handleClick(name: string, description: string) {
+    this.setState({
+      name: name,
+      description: description
+    })
+  }
+
   render() {
     return (
       <div>
         <div>chef: {this.props.chef}</div>
         <Register
-          name = 'ハンバーグ'
-          description = '肉をこねる'
+          name = {this.state.name}
+          description = {this.state.description}
+          onClick = {(name, description) => this.handleClick(name, description)}
         />
       </div>
     )
   }
 }
-
-// Main.defaultProps = {
-//   chef: "David",
-// }
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
